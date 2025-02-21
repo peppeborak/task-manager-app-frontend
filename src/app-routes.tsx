d0dbd752c2847c18e98552ade3f4916ce5e96f19
peppeborak <peppe@borak.se>
2025-02-21 18:49:40 +0100

    feat: add protected routes

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,14 +1,19 @@
 import { Navigate, Route, Routes } from 'react-router'
 import LoginPage from './pages/login-page'
 import SignupPage from './pages/signup-page'
-
+import { DashboardPage } from './pages/dashboard-page'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route index path="/login" element={<LoginPage />} />
-      <Route index path="/signup" element={<SignupPage />} />
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="*" element={<Navigate to="/login" />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Route>
     </Routes>
   )
 }
---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { jwtDecode } from 'jwt-decode'
+import { Navigate, Outlet } from 'react-router'
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token')
+  if (!token) return false
+
+  try {
+    const decoded: { exp: number } = jwtDecode(token)
+    return decoded.exp * 1000 > Date.now()
+  } catch (error) {
+    console.log('Token decoding failed', error);
+    return false;
+  }
+}
+
+export function ProtectedRoute() {
+  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />
+}
